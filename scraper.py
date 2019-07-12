87df13c9d6635a51651a08656a4efc725207c7ba
SCaicedo99 <43104210+SCaicedo99@users.noreply.github.com>
2019-07-12 14:12:39 -0400

    initial commit

---
+++ scraper.py
@@ -0,0 +1,52 @@
+# pip install request bs4
+import requests
+from bs4 import BeautifulSoup
+import smtplib # TODO find out more about this library
+
+URL = 'https://www.amazon.com/BIC-Round-Ballpoint-Medium-60-Count/dp/B0012YVGOW?ref_=Oct_' \
+      'DLandingS_PC_6a44c109_0&smid=ATVPDKIKX0DER&th=1&psc=1'
+
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+page = requests.get(URL, headers=headers)
+
+def check_price():
+    soup1 = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+
+    title = soup2.find(id="productTitle").get_text()
+    price = soup2.find(id="priceblock_ourprice").get_text()
+
+    converted_price = float(price[1:])
+    ideal_amount = 4.99
+    print(title.strip())
+    print(converted_price)
+
+    if converted_price > ideal_amount:
+        send_mail()
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo() # TODO read about ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('sebascaicedo25@gmail.com','vhhrbzhfcvoyffkz')
+
+    subject = 'Price fell down'
+    body = 'Check the amazon link: https://www.amazon.com/BIC-Round-Ballpoint-Medium-60-Count/dp/B0012YVGOW?ref_=Oct_' \
+           'DLandingS_PC_6a44c109_0&smid=ATVPDKIKX0DER&th=1&psc=1'
+
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail(
+        'sebascaicedo25@gmail.com',
+        'sebascaicedo-01@hotmail.com',
+        msg
+    )
+    print("EMAIL HAS BEEN SENT!")
+
+    server.quit()
+
+check_price()
